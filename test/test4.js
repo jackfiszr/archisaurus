1f7f2d294e4305a5146fe66a76fb1b8644670408
Stanislav Obrazcoff <obrazcoff@gmail.com>
2019-10-29 15:03:39 +0100

    Update tests

--- test/test4.js
+++ test/test4.js
@@ -3,30 +3,41 @@ const { database } = require('./lib/database2')
 const db = database
 
 
-const MAX_RANDOM = 1000000
+const MAX_COUNT_OF_ITEMS = 5
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
   }
   
 db.transactionsStart(() => {
-	for (let i = 0 ; i < MAX_RANDOM; i++) {
+	for (let i = 0 ; i < MAX_COUNT_OF_ITEMS; i++) {
 		db.post('coupons', { name: `coupon${i}`, count: getRandomInt(10) })
 	}
 })
 
 db.transactionsStart(() => {
-	for (let i = 0 ; i < 1000000; i++) {
-		db.post('users', { couponName: `coupon${getRandomInt(MAX_RANDOM)}` })
+	for (let i = 0 ; i < MAX_COUNT_OF_ITEMS; i++) {
+		db.post('users', { userName: `user${i}`,
+						   email: `user${i}@mail.com`,
+						   couponName: `coupon${getRandomInt(MAX_COUNT_OF_ITEMS)}` })
 	}
 
 })
 
-describe('hooks', function() {
+describe('Database for searchByIndex', function() {
 	describe('constructor', function() {
 		it('should create folders and setup basic environment', function() {
 			//console.table(db.search('coupons'))
 			assert.equal(true, true)
 		})
+	})
+})
+
+describe('searchByIndex', () => {
+	it('should find user0 by userNmae and email index', function() {		
+		assert.equal(
+			db.searchByIndex('users', {userName: 'user0', email: 'user0@mail.com'})[0].userName,
+			'user0'
+		)
 	})
 })
