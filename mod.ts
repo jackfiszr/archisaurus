6b05dddaf97e91dff4f4439d59462e650cb8a0c0
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-01-08 23:43:27 +0100

    feat: pass throw if missing record id

--- mod.ts
+++ mod.ts
@@ -2,6 +2,9 @@ import { ensureDirSync, join } from "./deps.ts";
 import config from "./config.ts";
 
 export function createRecord(record: { [key: string]: unknown }) {
+  if (!record.id) {
+    throw(Error("Record should contain an `id` property"))
+  }
   ensureDirSync(config.dbDir);
   const filePath = join(config.dbDir, `${record.id}.json`);
   Deno.writeTextFileSync(filePath, JSON.stringify(record));
