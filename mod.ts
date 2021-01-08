b768fc6db54ef35f47d9842f4cc2ff3e378f9406
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-01-08 23:14:39 +0100

    feat: pass record file contents parsable

--- mod.ts
+++ mod.ts
@@ -4,6 +4,5 @@ import config from "./config.ts";
 export function createRecord(record: { [key: string]: unknown }) {
   ensureDirSync(config.dbDir);
   const filePath = join(config.dbDir, `${record.id}.json`);
-  const res = Deno.createSync(filePath);
-  Deno.close(res.rid);
+  Deno.writeTextFileSync(filePath, "{}");
 }
