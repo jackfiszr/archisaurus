595e779ac9b67b1fa45efc4d80125ef5f57be323
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-02-02 16:30:49 +0100

    fix(deps): update to std 0.84.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export {
   ensureDirSync,
   existsSync,
-} from "https://deno.land/std@0.83.0/fs/mod.ts";
-export { join } from "https://deno.land/std@0.83.0/path/mod.ts";
+} from "https://deno.land/std@0.84.0/fs/mod.ts";
+export { join } from "https://deno.land/std@0.84.0/path/mod.ts";
 export { default as Ask } from "https://deno.land/x/ask@1.0.5/mod.ts";
